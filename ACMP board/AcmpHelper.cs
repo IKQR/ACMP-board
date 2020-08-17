e3531a5dbc22efe125679e30a5c80586af14d388
Ilya Kusik <kusikilya2001@outlook.com>
2020-08-17 17:01:23 +0300

    real-git-format

--- ACMP board/AcmpHelper.cs
+++ ACMP board/AcmpHelper.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Text;
-using System.IO;
+using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 using System.Net;
 using HtmlAgilityPack;
 
