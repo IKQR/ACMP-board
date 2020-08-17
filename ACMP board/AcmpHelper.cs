51e972764cc06024b6e4165b7daff028eee37559
Ilya Kusik <kusikilya2001@outlook.com>
2020-08-17 16:52:10 +0300

    git format

--- ACMP board/AcmpHelper.cs
+++ ACMP board/AcmpHelper.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Text;
-
 using HtmlAgilityPack;
 
 namespace ACMP_board
